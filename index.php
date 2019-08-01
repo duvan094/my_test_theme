b215daaa0614812b179adadc88ed0ecadc6ff82e
Jacob Duvander <duvan.der@live.se>
2019-08-01 21:07:23 +0200

    Added grid

--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <?php get_header(); ?>
 
 <main>
-
+    
     <?php 
         if ( have_posts() ) {
+        
+        echo "<ul class='grid'>";
+    
             while ( have_posts() ) {
                 the_post(); 
-
-
             
     ?>
-
-            <a href=<?php the_permalink();?>>
-                <h2>
-                    <?php the_title() ?>
-                </h2>
-            </a>
+            <li class="grid-item">
+                <a href=<?php echo the_permalink();?>>
+                    <figure style="background:url('<?php echo get_the_post_thumbnail_url($post->ID,'full') ?>'); background-size:cover; background-position:center;">
+                    </figure>
+                    <!--                        
+        <h2>
+                        <?php //the_title() ?>
+                    </h2>-->
+                </a>
+            </li>
     <?php
 
                 
             } // end while
+        
+        echo "</ul>";
+
         }
 
 
